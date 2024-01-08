08a6849b2d46bb910c380af536e1eeb916da9551
Vuong Dang <ddvuong@gmail.com>
2024-01-09 00:46:41 +0100

    Add simple header and text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from 'next/image'
+import { Container, Flex, Heading, Text } from '@radix-ui/themes'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+      <Container size="2">
+        <Flex direction="column">
+          <Heading color="indigo">NextJs - TailwindCss -  Radix UI Theme</Heading>
+          <Text color="gray">
+            This is a simple NextJs app with TailwindCss and Radix UI Theme.
+          </Text>
+        </Flex>
+      </Container>
     </main>
   )
 }
