eb2b676079af2a615fc3a96d7fd8929b4912427a
Vuong Dang <ddvuong@gmail.com>
2024-01-25 22:54:00 +0100

    Add radix-themes-tw dependency

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import { GetServerSideProps } from 'next';
+
+export default function About(){
+    return (
+        <div className='font-medium text-9 text-plum-7 text-center mt-rx-7'>
+            About me
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Container, Flex, Heading, Text } from '@radix-ui/themes'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -7,9 +8,9 @@ export default function Home() {
       <Container size="2">
         <Flex direction="column">
           <Heading color="indigo">NextJs - TailwindCss -  Radix UI Theme</Heading>
-          <Text color="gray">
-            This is a simple NextJs app with TailwindCss and Radix UI Theme.
-          </Text>
+          <Link href='/about' color="gray">
+            About me
+          </Link>
         </Flex>
       </Container>
     </main>
